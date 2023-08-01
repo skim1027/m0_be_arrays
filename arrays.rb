f7c615acf33097b72b7400eb144b514f2e1de4da
Sooyung Kim <kimx1279@gmail.com>
2023-08-01 15:53:35 -0600

    Add four arrays

---
+++ arrays.rb
@@ -0,0 +1,13 @@
+fav_artists = ["Rihanna", "Childish Gambino", "Ed Sheeran", "Camila Cabello"]
+significant_age = [14, 25, 26, 33]
+average_temp= [85.3, 84.7, 87.7, 87.5]
+true_or_false = [10 == 3, 15 > 3, 9 != 4, 6 < 19]
+
+puts fav_artists
+puts
+puts significant_age
+puts
+puts average_temp
+puts
+puts true_or_false
+
